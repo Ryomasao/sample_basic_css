dae47eb3eff9d0a39567a3cebe89ed6af8337782
tohu.masao <konoemario@gmail.com>
2017-09-25 23:45:11 +0900

    toggleButton

--- routes/sample.js
+++ routes/sample.js
@@ -6,6 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('sample_common', { title: 'CSS練習用のTOPページ' });
 });
 
+router.get('/2', function(req, res, next) {
+  //var path = 'sample_2/' + req.params.pageTitle;
+  //console.log(path);
+  //res.render('sample_2/'+ req.params.pageTitle , { title: 'sample'},function(err,html){
+  res.render('sample_2' , { title: 'sample'},function(err,html){
+    if(!err){
+      res.send(html)
+    }else{
+      res.render('sample_common/error', {title:'みつからん'});
+    }
+  });
+});
+
 router.get('/:sample_id', function(req, res, next) {
   var sampleId = req.params.sample_id;
   res.render('sample_' + sampleId, { title: 'sample' + sampleId},function(err,html){
@@ -16,5 +29,6 @@ router.get('/:sample_id', function(req, res, next) {
     }
   });
 });
+
 
 module.exports = router;
